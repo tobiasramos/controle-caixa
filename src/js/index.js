4f211584ab5f1bd49a2a43a031d0027f65b43583
Tobias <tobiasramossilva@gmail.com>
2023-03-09 19:43:33 -0300

    Exibindo os campos dos inputs preenchidos (exceto os de checkbox) com o dom.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,11 @@
+const v_amount = document.querySelector("#amount")
+const v_date = document.querySelector("#date")
+const v_observation = document.querySelector("#observation")
+const v_revenue = document.querySelector("#revenue")
+const v_expense = document.querySelector("#expense")
 const btn = document.querySelector(".btn-add")
+const test =document.querySelector("#root")
 
 btn.addEventListener("click",() => {
-    alert("Funcinou")
+    test.innerHTML = `<p>Valor: ${v_amount.value} Data: ${v_date.value} Observação:${v_observation.value} Tipo da movimentação: </p>`
 })
