cef3d09e2c7170f34ed04c100d7e2e19b57aeef2
Tobias <tobiasramossilva@gmail.com>
2023-03-16 16:33:08 -0300

    Pegando os elementos que vão ser manipulados no DOM

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+const v_amount = document.getElementById("amount")
+const v_date = document.getElementById("date")
+const v_observation = document.getElementById("observation")
+const v_revenue = document.getElementById("revenue")
+const v_expense = document.getElementById("expense")
+const btn_add = document.getElementById("btn-add")
+const exhibition = document.getElementById("exhibition")
+const tb = document.getElementById("tb")
+
+
+btn_add.addEventListener("click", () => {
+    alert("Funcionou")
+})
