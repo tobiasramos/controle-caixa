b95b2b4e74057a1724e8c86fe0f4794b63948586
Tobias <tobiasramossilva@gmail.com>
2023-03-16 16:44:34 -0300

    Mostrando os dados digitado pelo usuario na tela de exibição, porém, ainda falta ele escolher o tipo da movimentação e o botão de excluir

--- src/js/index.js
+++ src/js/index.js
@@ -9,5 +9,17 @@ const tb = document.getElementById("tb")
 
 
 btn_add.addEventListener("click", () => {
-    alert("Funcionou")
+    let qtdRows = tb.rows.length
+    let row = tb.insertRow(qtdRows)
+    let amount = row.insertCell(0)
+    let date = row.insertCell(1)
+    let observation = row.insertCell(2)
+    let typeMovement = row.insertCell(3)
+    let action = row.insertCell(4)
+
+    amount.innerHTML = v_amount.value
+    date.innerHTML = v_date.value
+    observation.innerHTML = v_observation.value
+    typeMovement.innerHTML = v_amount.value
+    action.innerHTML = "Excluir"
 })
